03092ee3fbce0797eea246dfc17135854a0c6235
naod234 <D295700@edu.curio.nl>
2025-03-12 13:46:34 +0100

    img
    image een welkom text

--- index.php
+++ index.php
@@ -9,8 +9,14 @@
 <body>
     
     <div class="container">
-
-
+    <div class="hero">
+            <img src="img/logo-big-v3(1).png" alt="Pretpark Logo" class="logo">
+            <h1>Welkom bij het Pretpark Takensysteem</h1>
+            <p class="welcome-text">
+                Beheer hier alle taken voor het pretpark. 
+                Maak nieuwe taken aan, werk bestaande taken bij en houd overzicht over alle afdelingen.
+            </p>
+        </div>
     </div>
 
 </body>
