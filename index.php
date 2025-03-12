cd76220281f8709d29ac3548814da19322b362bb
naod234 <D295700@edu.curio.nl>
2025-03-12 14:00:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,13 @@
 <body>
     
     <div class="container">
-    <div class="hero">
-            <img src="image/logo-big-v3 (1).png" alt="Pretpark Logo">
+        
             <h1>Welkom bij het Pretpark Takensysteem</h1>
             <p class="welcome-text">
                 Beheer hier alle taken voor het pretpark. 
                 Maak nieuwe taken aan, werk bestaande taken bij en houd overzicht over alle afdelingen.
             </p>
+            <img src="image/logo-big-v3 (1).png" alt="Pretpark Logo">
         </div>
     </div>
 
